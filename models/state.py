8410cf04c85131a362f4a1faaf22e92c302b572a
Cristian Betancur <34930340+cristiandbetancurg@users.noreply.github.com>
2021-02-17 23:57:18 -0400

    new´s files console

---
+++ models/state.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+[State class]
+"""
+from models.base_model import BaseModel
+
+
+class State(BaseModel):
+    """
+    [State]
+
+    Args:
+        BaseModel ([class]): class that inherited by State
+    """
+    name = ""
---
+++ models/user.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+[User class] public class that inherits from BaseModel
+"""
+from models.base_model import BaseModel
+
+
+class User(BaseModel):
+    """
+    [User]
+
+    Args:
+        BaseModel ([class]): class biherited by User
+    """
+    email = ""
+    password = ""
+    first_name = ""
+    last_name = ""
